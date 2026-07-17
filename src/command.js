3fc5b1408589576e83059ce834c4c257b3034794
subikstha <subsatch@gmail.com>
2026-07-17 17:28:03 +0545

    feat: create server to serve notes to web

--- src/command.js
+++ src/command.js
@@ -7,6 +7,7 @@ import {
   removeNote,
   removeAllNotes,
 } from "./notes.js";
+import { start } from "./server.js";
 
 const listNotes = (notes) => {
   notes.forEach((note) => {
@@ -91,7 +92,10 @@ yargs(hideBin(process.argv))
         type: "number",
       });
     },
-    async (argv) => {},
+    async (argv) => {
+      const notes = await getAllNotes();
+      start(notes, argv.port);
+    },
   )
   .command(
     "clean",
---
+++ src/server.js
@@ -0,0 +1,47 @@
+import fs from "node:fs/promises";
+import http from "node:http";
+import open from "open";
+import { fileURLToPath } from "node:url";
+
+const interpolate = (html, data) => {
+  return html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, placeholder) => {
+    return data[placeholder] || "";
+  });
+};
+
+const formatNotes = (notes) => {
+  return notes
+    .map((note) => {
+      return `
+      <div class="note">
+        <p>${note.content}</p>
+        <div class="tags">
+          ${note.tags.map((tag) => `<span class="tag">${tag}</span>`).join("")}
+        </div>
+      </div>
+    `;
+    })
+    .join("\n");
+};
+
+const createServer = (notes) => {
+  return http.createServer(async (req, res) => {
+    const HTML_PATH = fileURLToPath(
+      new URL("./template.html", import.meta.url),
+    );
+    const template = await fs.readFile(HTML_PATH, "utf-8");
+    const html = interpolate(template, { notes: formatNotes(notes) });
+
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(html);
+  });
+};
+
+export const start = (notes, port) => {
+  const server = createServer(notes);
+  server.listen(port, () => {
+    const address = `http:localhost:${port}`;
+    console.log(`server on port ${address}`);
+    open(address);
+  });
+};
