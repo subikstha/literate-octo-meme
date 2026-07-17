ec499258ec0b2a0ca783526656a14acfada96f1b
subikstha <subsatch@gmail.com>
2026-07-17 16:57:19 +0545

    feat: http server

---
+++ server.js
@@ -0,0 +1,11 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from node.js");
+});
+
+server.listen(4000, () => {
+  console.log("server running on port 4000");
+});
--- tests/notes.test.js
+++ tests/notes.test.js
@@ -15,15 +15,40 @@ beforeEach(() => {
   saveDB.mockClear();
 });
 
-test("newNote inserts data and returns it", async () => {
-  const aNote = {
-    content: "this is my note",
-    id: 1,
-    tags: ["hello"],
-  };
+describe("cli app", () => {
+  test("newNote inserts data and returns it", async () => {
+    const aNote = {
+      content: "this is my note",
+      id: 1,
+      tags: ["hello"],
+    };
 
-  insert.mockResolvedValue(aNote);
-  const result = await newNote(aNote.content, aNote.tags);
-  expect(result.content).toEqual(aNote.content);
-  expect(result.tags).toEqual(aNote.tags);
+    insert.mockResolvedValue(aNote);
+    const result = await newNote(aNote.content, aNote.tags);
+    expect(result.content).toEqual(aNote.content);
+    expect(result.tags).toEqual(aNote.tags);
+  });
+
+  test("getAllNotes returns all notes", async () => {
+    const db = {
+      notes: ["note1", "note2", "note3"],
+    };
+    getDB.mockResolvedValue(db);
+
+    const result = await getAllNotes();
+    expect(result).toEqual(db.notes);
+  });
+
+  test("removeNote does nothing if id is not found", async () => {
+    const notes = [
+      { id: 1, content: "note 1" },
+      { id: 2, content: "note 2" },
+      { id: 3, content: "note 3" },
+    ];
+    saveDB.mockResolvedValue(notes);
+
+    const idToRemove = 4;
+    const result = await removeNote(idToRemove);
+    expect(result).toBeUndefined();
+  });
 });
