cc69ae2234b1a728a3da841c10efb1b506a8b550
subikstha <subsatch@gmail.com>
2026-07-17 14:03:08 +0545

    feat: added a json file as a ddb

---
+++ src/db.js
@@ -0,0 +1,23 @@
+import fs from "node:fs";
+
+// import.meta.url = file:///home/subikstha/projects/frontend%20masters/notes-cli/src/db.js
+// DB_PATH = /home/subikstha/frontend%20masters/notes-cli/src/db.json
+
+const DB_PATH = new URL("../db.json", import.meta.url).pathname;
+
+export const getDB = async () => {
+  const db = await fs.readFile(DB_PATH, "utf-8");
+  return JSON.parse(db);
+};
+
+export const saveDB = async () => {
+  await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
+  return db;
+};
+
+export const insert = async (data) => {
+  const db = await getDB();
+  db.notes.push(data);
+  await saveDB(db);
+  return data;
+};
